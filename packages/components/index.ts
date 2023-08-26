26ca0c63c229e22463c688a24be91ab481f037ba
Lazy <1282394741@qq.com>
2023-08-26 09:46:06 +0800

    组件名添加

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,4 +1,5 @@
 import { type App, Component } from 'vue';
+import { camelize } from '../utils/index';
 import './index.css';
 const modulesFiles = import.meta.glob('./dd-*/index.ts', { import: 'default', eager: true });
 
@@ -10,6 +11,7 @@ export default {
 		for (const path in modulesFiles) {
 			const mode: Component = modulesFiles[path];
 			app.component(mode.name, mode);
+			app.component(camelize(`-${mode.name}`), mode);
 		}
 	},
 };
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,17 +1,16 @@
-import type { App, Component } from "vue";
+import type { App, Component } from 'vue';
 const camelizeRE = /-(\w)/g;
-const camelize = (str: string): string =>
-    str.replace(camelizeRE, (_, c) => c.toUpperCase());
+const camelize = (str: string): string => str.replace(camelizeRE, (_, c) => c.toUpperCase());
 
 const install = <T extends Component>(component: T) => {
-    (component as Record<string, unknown>).install = (app: App) => {
-        const { name } = component;
-        if (name) {
-            app.component(name, component);
-            app.component(camelize(`-${name}`), component);
-        }
-    };
-    return component;
+	(component as Record<string, unknown>).install = (app: App) => {
+		const { name } = component;
+		if (name) {
+			app.component(name, component);
+			app.component(camelize(`-${name}`), component);
+		}
+	};
+	return component;
 };
 
-export { install };
+export { install, camelize };
