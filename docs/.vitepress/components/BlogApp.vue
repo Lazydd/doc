b7cef69d6e51d8b0d8cc17d46b7631308466a26d
lazy <ddlazy@qq.com>
2023-09-20 17:56:10 +0800

    更新

---
+++ docs/.vitepress/components/BlogApp.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import Theme from 'vitepress/theme';
+defineOptions({ name: 'BlogApp' });
+
+const { Layout } = Theme;
+</script>
+
+<template>
+	<Layout>
+		<template #layout-top></template>
+		<template #doc-before>
+			<slot name="doc-before" />
+		</template>
+
+		<!-- 评论 -->
+		<template #doc-after>
+			<slot name="doc-after" />
+		</template>
+
+		<!-- 透传默认主题的其它插槽 -->
+		<!-- navbar -->
+		<template #nav-bar-title-before><slot name="nav-bar-title-before" /></template>
+		<template #nav-bar-title-after><slot name="nav-bar-title-after" /></template>
+		<template #nav-bar-content-after><slot name="nav-bar-content-after" /></template>
+		<template #nav-screen-content-before><slot name="nav-screen-content-before" /></template>
+		<template #nav-screen-content-after><slot name="nav-screen-content-after" /></template>
+
+		<!-- sidebar -->
+		<template #sidebar-nav-before><slot name="sidebar-nav-before" /></template>
+
+		<!-- content -->
+		<template #page-top><slot name="page-top" /></template>
+		<template #page-bottom><slot name="page-bottom" /></template>
+
+		<template #not-found><slot name="not-found" /></template>
+		<template #home-hero-info><slot name="home-hero-info" /></template>
+		<template #home-hero-image><slot name="home-hero-image" /></template>
+		<template #home-hero-after><slot name="home-hero-after" /></template>
+		<template #home-features-before><slot name="home-features-before" /></template>
+		<template #home-features-after><slot name="home-features-after" /></template>
+
+		<template #doc-footer-before><slot name="doc-footer-before" /></template>
+
+		<template #doc-top><slot name="doc-top" /></template>
+		<template #doc-bottom><slot name="doc-bottom" /></template>
+
+		<template #aside-top><slot name="aside-top" /></template>
+		<template #aside-bottom><slot name="aside-bottom" /></template>
+		<template #aside-outline-before><slot name="aside-outline-before" /></template>
+		<template #aside-outline-after><slot name="aside-outline-after" /></template>
+		<template #aside-ads-before><slot name="aside-ads-before" /></template>
+		<template #aside-ads-after><slot name="aside-ads-after" /></template>
+	</Layout>
+</template>
+<style scoped lang="less">
+.home {
+	margin: 0 auto;
+	padding: 20px;
+	max-width: 1126px;
+}
+
+@media screen and (min-width: 960px) {
+	.home {
+		padding-top: var(--vp-nav-height);
+	}
+}
+
+.header-banner {
+	width: 100%;
+	padding: 60px 0;
+}
+
+.content-wrapper {
+	display: flex;
+	align-items: flex-start;
+	justify-content: center;
+}
+
+.blog-list-wrapper {
+	width: 100%;
+}
+.blog-info-wrapper {
+	margin-left: 16px;
+	position: sticky;
+	top: 100px;
+}
+
+@media screen and (max-width: 959px) {
+	.blog-info-wrapper {
+		margin-left: 16px;
+		position: sticky;
+		top: 40px;
+	}
+}
+
+@media screen and (max-width: 767px) {
+	.content-wrapper {
+		flex-wrap: wrap;
+	}
+
+	.blog-info-wrapper {
+		margin: 20px 0;
+		width: 100%;
+	}
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,21 +4,28 @@ import Demo from 'vitepress-theme-demoblock-fork/dist/client/components/Demo.vue
 import DemoBlock from 'vitepress-theme-demoblock-fork/dist/client/components/DemoBlock.vue';
 import 'vitepress-theme-demoblock-fork/dist/theme/styles/index.css';
 
+import ZoomImg from '../components/ZoomImg.vue';
+import BlogApp from '../components/BlogApp.vue'
+
 import '../style/main.css';
 import '../style/vars.css';
 import '../style/scroll.css';
+import '../style/inline-demo.css';
+
 // import { ddButton, ddIcon } from '@/lib/ddlazy-ui-plus.es';
 import ddlazyUi from '@/lib/ddlazy-ui-plus.es.js';
 import '@/lib/index.css';
 import IconList from '../../src/basic/icon/icon.vue';
 export default {
 	...DefaultTheme,
+	Layout: BlogApp,
 	enhanceApp: (ctx) => {
 		const { app } = ctx;
 		DefaultTheme.enhanceApp(ctx);
 		app.component('Demo', Demo);
 		app.component('IconList', IconList);
 		app.component('DemoBlock', DemoBlock);
+		app.component('Image', ZoomImg);
 		// app.use(ddButton);
 		// app.use(ddIcon);
 		app.use(ddlazyUi);
